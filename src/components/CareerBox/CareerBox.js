c3542264d7e956bebf3cd4a398223969c7810c43
Shubham Sharma <im_shubhamsharma@outlook.com>
2022-11-30 17:21:16 +0530

    fixed responsiveness on faq page

--- src/components/CareerBox/CareerBox.js
+++ src/components/CareerBox/CareerBox.js
@@ -7,40 +7,38 @@ import {CareerBox} from "./style"
 
 const Career = ({space}) => {
     return (
-        <CareerBox className={`flex md:flex-row flex-col py-10 xl:px-10 px-10 justify-between rounded-xl xl:items-center Carrer_div ${space}`}>
-            <div className="md:w-[20%]">
-                <h4 className="dark:text-black text-white text-4xl leading-normal font-extrabold">
-                    Looking for career opportunity?
-                </h4>
+      <CareerBox
+        className={`flex md:flex-row flex-col py-10 xl:px-10 px-10 justify-between rounded-xl xl:items-center Carrer_div ${space}`}
+      >
+        <div className="md:w-[20%]">
+          <h4 className="dark:text-black text-white text-4xl leading-normal font-extrabold">
+            Looking for career opportunity?
+          </h4>
+        </div>
+        <div className="xl:w-[20%] xl:mt-0 mt-3 dark:text-black text-white gap-5 leading-loose">
+          <div className="flex justify-between">
+            <div className="flex  items-center font-bold">
+              <BsChatSquareText /> <span className="ml-2">Email</span>
             </div>
-            <div className="xl:w-[20%] xl:mt-0 mt-3 dark:text-black text-white gap-5 leading-loose">
-                <div className="flex justify-between">
-                    <div className="flex  items-center font-bold">
-                        <BsChatSquareText /> <span className="ml-2">Email</span>
-                    </div>
-                    <div className="w-[70%]">
-                        <a href="mailto:contact@threearrow.in">
-                        contact@threearrow.in
-                        </a>
-                    </div>
-                </div>
-                <div className="flex justify-between ">
-                    <span className="flex w-[20%] items-center  font-bold">
-                        <FiPhoneCall /> <span className="ml-2">Call</span>
-                    </span>
-                    <div className="w-[70%]">+91 1234567890</div>
-                </div>
+            <div className="w-[70%] font-medium">
+              <a href="mailto:contact@threearrow.in">contact@threearrow.in</a>
             </div>
-            <div className="md:w-1/5 md:mt-0 mt-3 flex xl:justify-end xl:mt-0 mt-12 items-center">
-                <div
-                    className="bg-white flex justify-around rounded-full p-2 w-80 cursor-pointer delay-75 duration-700 transition-all text-black hover:bg-black hover:text-white dark:bg-black dark:text-white dark:hover:bg-white dark:hover:text-black"
-                >
-                    <button className="font-extrabold ">Explore more</button>
-                    <BsArrowRight className="text-5xl" />
-                </div>
-            </div>
-        </CareerBox>
-    )
+          </div>
+          <div className="flex justify-between ">
+            <span className="flex w-[20%] items-center  font-bold">
+              <FiPhoneCall /> <span className="ml-2">Call</span>
+            </span>
+            <div className="w-[70%] font-medium">+91 1234567890</div>
+          </div>
+        </div>
+        <div className="md:w-1/5 md:mt-0 mt-3 flex xl:justify-end xl:mt-0 mt-12 items-center">
+          <div className="bg-white flex justify-around rounded-full p-2 w-80 cursor-pointer delay-75 duration-700 transition-all text-black hover:bg-black hover:text-white dark:bg-black dark:text-white dark:hover:bg-white dark:hover:text-black">
+            <button className="font-extrabold ">Explore more</button>
+            <BsArrowRight className="text-5xl" />
+          </div>
+        </div>
+      </CareerBox>
+    );
 }
 
 export default Career;
--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -6,18 +6,18 @@ import TwoSection from "../TwoSection/TwoSection";
 const FAQ = () => {
 
   return (
-    <div className="mt-16">
+    <div className="mt-16 px-[16px] md:px-0">
       <Title className="font-extrabold xl:text-7xl xl:leading-tight tracking-tighter py-4 ">
         Frequently Asked Questions
       </Title>
-      <p className="xl:text-3xl text-1xl xl:leading-normal dark:text-[#81AFDD] text-black w-6/12 mb-20">
+      <p className="xl:text-3xl text-1xl xl:leading-normal dark:text-[#81AFDD] text-black md:w-6/12 mb-20">
         Everything you need to know about the product and billing. Can't find
         the answer you are looking for? Please{" "}
         <Link href="contact">
           <span className="underline">contact us.</span>
         </Link>
       </p>
-      <div className="grid grid-cols-3 gap-20 my-20">
+      <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-20 my-20">
         {data.FAQ.Development.map((item, index) => {
           return (
             <div key={index}>
